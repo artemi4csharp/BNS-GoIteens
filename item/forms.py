f3b23f9de9faaaede18b86c4616c6ca7cd4c0a29
yyaaaarriikk <lavrenjuk0379@gmail.com>
2025-09-21 12:02:29 +0300

    Змiнив форми редагування

--- item/forms.py
+++ item/forms.py
@@ -1,5 +1,5 @@
 from django.forms import forms
-from django.forms import modelform_factory, inlineformset_factory, modelformset_factory
+from django.forms import modelform_factory
 from bns_goiteens.models import Category, Location, Item, Service, Rating, Promotion, SavedItem, Message
 
 CategoryForm = modelform_factory(
@@ -20,12 +20,10 @@ ItemCreationForm = modelform_factory(
     labels = {'name': 'Назва', 'decription': 'Опис', 'price': 'Ціна', 'category':'Категорія', 'owner': 'Власник', 'location': 'Розміщення', 'image': 'Фото'}, 
 )
 
-ItemEditForm = modelformset_factory(
-    Item, 
-    fields = ['name', 'description', 'price', 'image'],
-    extra = 1, 
-    can_delete = True
-    )
+class ItemEditForm(forms.ModelForm):
+    class Meta: 
+        model=Item
+        fields = ['name', 'description', 'price', 'is_active', 'image']
 
 ServiceCreationForm = modelform_factory(
     Service, 
@@ -33,13 +31,10 @@ ServiceCreationForm = modelform_factory(
     labels = {'name': 'Назва', 'description': 'Опис', 'price': 'Ціна', 'category':'Категорія', 'owner': 'Власник', 'location': 'Розміщення', 'image': 'Фото'}, 
 )
 
-ServiceEditForm = modelformset_factory(
-    Service, 
-    fields = ['name', 'description', 'price', 'image'],
-    extra = 1, 
-    can_delete = True
-    )
-
+class ServciceEditForm(forms.ModelForm):
+    class Meta:
+        model = Service, 
+        fields = ['name', 'description', 'price', 'image']
 
 class RatingForm(forms.ModelForm):
     class Meta: 
