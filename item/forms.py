bda456b5d0e9cc1b9a9a218bbc729bab2908aa48
yyaaaarriikk <lavrenjuk0379@gmail.com>
2025-09-27 14:26:15 +0300

    commit before the merge

---
+++ item/forms.py
@@ -0,0 +1,63 @@
+from django.forms import forms
+from django.forms import modelform_factory
+from bns_goiteens.models import Category, Location, Item, Service, Rating, Promotion, SavedItem, Message
+
+CategoryForm = modelform_factory(
+    Category, 
+    fields = ['name', 'is_active'],
+    labels = {'name':'Назва товару', 'is_active': 'Чи активна категорія'}
+)
+
+LocationForm = modelform_factory(
+    Location, 
+    fields = ['city', 'region', 'country'],
+    labels = {'city':'Місто', 'region':'Область', 'country': 'Країна'}
+)
+
+ItemCreationForm = modelform_factory(
+    Item, 
+    fields = ['name', 'description', 'price', 'category', 'owner', 'location', 'image'],
+    labels = {'name': 'Назва', 'decription': 'Опис', 'price': 'Ціна', 'category':'Категорія', 'owner': 'Власник', 'location': 'Розміщення', 'image': 'Фото'}, 
+)
+
+class ItemEditForm(forms.ModelForm):
+    class Meta: 
+        model=Item
+        fields = ['name', 'description', 'price', 'is_active', 'image']
+
+ServiceCreationForm = modelform_factory(
+    Service, 
+    fields = ['name', 'description', 'price', 'category', 'owner', 'location', 'image'],
+    labels = {'name': 'Назва', 'description': 'Опис', 'price': 'Ціна', 'category':'Категорія', 'owner': 'Власник', 'location': 'Розміщення', 'image': 'Фото'}, 
+)
+
+class ServciceEditForm(forms.ModelForm):
+    class Meta:
+        model = Service, 
+        fields = ['name', 'description', 'price', 'image']
+
+class RatingForm(forms.ModelForm):
+    class Meta: 
+        model = Rating
+        fields = ['value'],
+        widgets = {
+            'value': forms.RadioSelect(choices=[(i, '⭐' * i) for i in range(1, 6)])
+        }
+
+PromotionCreationForm = modelform_factory(
+    Promotion, 
+    fields = ['name', 'description', 'start_date', 'end_date', 'is_active'],
+    labels = {'name': 'Назва', 'description': 'Опис', 'start_date': 'Дата початку', 'end_date': 'Дата закінчення', 'is_active': 'Чи активне'}
+)
+
+SavedItemCreationForm = modelform_factory(
+    SavedItem, 
+    fields = ['object_id'],
+    labels = {'object_id': 'ID товару'} 
+)
+
+MessageCreationForm = modelform_factory(
+    Message, 
+    fields = ['content', 'receiver', 'read'],
+    labels = {'content': 'Вміст', 'receiver': 'отримувач', 'read': 'Прочитано'}
+)
