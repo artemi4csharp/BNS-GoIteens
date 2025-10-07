2e8db8bcb0e7cbafde840ac690068db40ea79380
MaxKorjik <koshchinmaxim06@gmail.com>
2025-10-07 15:08:07 +0300

    добавил редирект на главную страницу

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/bns/home/', permanent=True)),
     path('admin/', admin.site.urls),
     path('item/', include('item.urls')),
     path('bns/', include("bns_goiteens.urls")),
